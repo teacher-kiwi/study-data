7c8e4f79673d8a59b70486740a4107b995ac5e67
teacher-kiwi <kwi07@hanmail.net>
2022-09-09 20:28:20 +0900

    csrf trusted origins 수정

--- taebbong_drf/02_PhotoApp/myweb/settings.py
+++ taebbong_drf/02_PhotoApp/myweb/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [".ssambox.com", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ['.ssambox.com']
+CSRF_TRUSTED_ORIGINS = ['http://study.ssambox.com']
 
 # Application definition
 
