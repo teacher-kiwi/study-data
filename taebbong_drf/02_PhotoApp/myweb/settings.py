9ab952d1abac26f6de2627d9c2432c1d413ac6ca
teacher-kiwi <kwi07@hanmail.net>
2022-09-09 20:03:04 +0900

    allowed hosts, csrf trusted origins 수정

--- taebbong_drf/02_PhotoApp/myweb/settings.py
+++ taebbong_drf/02_PhotoApp/myweb/settings.py
@@ -25,8 +25,9 @@ SECRET_KEY = 'django-insecure-38xy8sk*6bxed0cudfk99hzx!qa=oe0wyvs9@c5l(1@u3mabi_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://study.ssambox.com", "127.0.0.1"]
+ALLOWED_HOSTS = [".ssambox.com", "127.0.0.1"]
 
+CSRF_TRUSTED_ORIGINS = ['.ssambox.com']
 
 # Application definition
 
