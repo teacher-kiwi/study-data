cc31996166e1645a56fcd0f5b797beb430be1932
teacher-kiwi <kwi07@hanmail.net>
2022-10-16 12:15:36 +0900

    챕터 6 완료

--- taebbong_drf/06_MyBoard/myboard/settings.py
+++ taebbong_drf/06_MyBoard/myboard/settings.py
@@ -135,7 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static/"
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
+STATIC_ROOT = "/var/www/study.ssambox.com/static/"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media/'
 
--- taebbong_drf/06_MyBoard/myboard/urls.py
+++ taebbong_drf/06_MyBoard/myboard/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include("users.urls")),
     path("", include("posts.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
