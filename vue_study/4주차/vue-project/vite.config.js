b5ca904cc42b3d306aed7d4c16f1039604b46fb5
teacher-kiwi <kwi07@hanmail.net>
2022-11-20 18:53:56 +0900

    vue 빌드 수정

--- vue_study/4주차/vue-project/vite.config.js
+++ vue_study/4주차/vue-project/vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue_study/4주차/vue-project/dist1/",
+  // base: "/study-data/vue_study/4주차/vue-project/dist1/",
   plugins: [vue()],
   resolve: {
     alias: {
