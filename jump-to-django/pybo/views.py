c098b959a842917f27e2fba42bd0b7d19f89f5e6
kiwi <kwi07@hanmail.net>
2023-09-21 14:23:58 +0900

    2-6 데이터 저장 완료

--- jump-to-django/pybo/views.py
+++ jump-to-django/pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Question
+from .models import Question, Answer
 
 
 def index(request):
@@ -13,3 +14,10 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {"question": question}
     return render(request, "pybo/question_detail.html", context)
+
+
+def answer_create(request: HttpRequest, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    answer = Answer(question=question, content=request.POST.get("content"))
+    answer.save()
+    return redirect("pybo:detail", question_id=question_id)
