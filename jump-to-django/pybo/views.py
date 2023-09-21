ee6a1848d7cc8745b988ddd3f46ea25230f36fd4
kiwi <kwi07@hanmail.net>
2023-09-21 17:03:15 +0900

    2-10 폼 완료

--- jump-to-django/pybo/views.py
+++ jump-to-django/pybo/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Question, Answer
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -17,7 +18,25 @@ def detail(request, question_id):
 
 
 def answer_create(request: HttpRequest, question_id):
+    if request.method != "POST":
+        return HttpResponseNotAllowed("Only POST is possible.")
     question = get_object_or_404(Question, pk=question_id)
-    answer = Answer(question=question, content=request.POST.get("content"))
-    answer.save()
-    return redirect("pybo:detail", question_id=question_id)
+    form = AnswerForm(request.POST)
+    if form.is_valid():
+        answer: Answer = form.save(commit=False)
+        answer.question = question
+        answer.save()
+        return redirect("pybo:detail", question_id=question.id)
+    context = {"question": question, "form": form}
+    return render(request, "pybo/question_detail.html", context)
+
+
+def question_create(request: HttpRequest):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("pybo:index")
+    else:
+        form = QuestionForm()
+    return render(request, "pybo/question_form.html", {"form": form})
