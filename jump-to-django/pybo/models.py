6ce108e12cc29b2856db868bf5219ecc0ff47e46
kiwi <kwi07@hanmail.net>
2023-09-21 12:21:39 +0900

    2-2 모델 완료

--- jump-to-django/pybo/models.py
+++ jump-to-django/pybo/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    subject = models.CharField(help_text="질문의 제목", max_length=200)
+    content = models.TextField(help_text="질문의 내용")
+    create_date = models.DateTimeField(help_text="질문을 작성한 일시", auto_now_add=True)
+
+    class Meta:
+        db_table = "question"
+
+    def __str__(self):
+        return self.subject
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(
+        help_text="질문", to="Question", on_delete=models.CASCADE
+    )
+    content = models.TextField(help_text="답변의 내용")
+    create_date = models.DateTimeField(help_text="답변을 작성한 일시", auto_now_add=True)
+
+    class Meta:
+        db_table = "answer"
