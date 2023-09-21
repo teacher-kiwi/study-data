ea1c7db535c7ed2b87d554b24fc8e6333273dccf
kiwi <kwi07@hanmail.net>
2023-09-21 12:59:20 +0900

    2-4 조회와 템플릿 완료

--- jump-to-django/pybo/views.py
+++ jump-to-django/pybo/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요. pybo입니다.")
+    question_list = Question.objects.order_by("-id")
+    context = {"question_list": question_list}
+    return render(request, "pybo/question_list.html", context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {"question": question}
+    return render(request, "pybo/question_detail.html", context)
